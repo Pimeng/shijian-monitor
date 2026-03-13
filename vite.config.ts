5f80d1884a2cae054529db0c4dc9d9c305fcc489
皮梦 <pimeng@vip.qq.com>
2026-03-13 10:24:46 +0800

    fix: 预转换字体文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig, type Plugin } from "vite"
-import ttf2woff2 from "ttf2woff2"
-import fs from "fs"
-
-function ttfToWoff2Plugin(): Plugin {
-  return {
-    name: 'vite-plugin-ttf2woff2',
-    enforce: 'post',
-    writeBundle() {
-      const fontsDir = './public/assets/fonts'
-      
-      if (!fs.existsSync(fontsDir)) return
-
-      function processDir(dir: string) {
-        fs.readdirSync(dir).forEach(file => {
-          const fullPath = path.join(dir, file)
-          const stat = fs.statSync(fullPath)
-
-          if (stat.isDirectory()) {
-            processDir(fullPath) // 递归处理子目录
-          } else if (file.endsWith('.ttf')) {
-            const woff2Path = fullPath.replace('.ttf', '.woff2')
-            
-            try {
-              const ttfBuffer = fs.readFileSync(fullPath)
-              const woff2Buffer = ttf2woff2(ttfBuffer)
-              fs.writeFileSync(woff2Path, woff2Buffer)
-              console.log(`✓ Converted: ${file} → ${path.basename(woff2Path)}`)
-            } catch (err) {
-              console.error(`✗ Failed: ${file}`, err)
-            }
-          }
-        })
-      }
-
-      processDir(fontsDir)
-    }
-  }
-}
-
+import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [
-    react(), ttfToWoff2Plugin()
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
